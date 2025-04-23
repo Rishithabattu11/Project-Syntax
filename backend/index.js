842e509ea53679515716a1d04f9ec112c7e8029f
abhiramseshu <162886598+abhiramseshu@users.noreply.github.com>
2025-04-24 00:04:55 +0530

    Normalize line endings

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
+const port = 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
