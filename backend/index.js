23caeef9e6efb2bda98d01aac9a94445c1ea2982
abhiramseshu <162886598+abhiramseshu@users.noreply.github.com>
2025-04-27 20:36:41 +0530

    added Codechef rating handler

--- backend/index.js
+++ backend/index.js
@@ -17,3 +17,30 @@ function started() {
   console.log(`Server started on port ${port}`);
 }
 app.listen(port, started);
+
+var sendgetCodeChefRatingObj = {
+  method: "GET",
+};
+
+async function getCodeChefRating(username) {
+  let result = -1;
+  let url = `https://codechef-api.vercel.app/handle/${username}`;
+  let fecthResult = await fetch(url, sendgetCodeChefRatingObj).catch(
+    (error) => {
+      console.error("Error:", error);
+      return -1;
+    }
+  );
+  if (fecthResult.status != 200) {
+    return -1;
+  }
+  result = await fecthResult.json();
+  if (result.status == 404) {
+    return -1;
+  }
+  return result.currentRating;
+}
+
+var username = "battu";
+
+var val = await getCodeChefRating(username).then((result) => {});
